efcc6df8d4f721f06be598fd2773fc67d05700e4
ConsolattaChepchirchir <consochep97@gmail.com>
2023-03-20 20:14:09 +0300

    complete array filter assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(driversArray,about){
+    return driversArray.filter(item=>{
+        return item.toUpperCase()===about.toUpperCase();
+    })
+}
+const fuzzyMatch=function(drivers,about){
+   return drivers.filter(drivers=>{
+       return drivers.toUpperCase().substring(0,about.length)===about.toUpperCase()
+   })
+}
+const matchName=(drivers,about)=>{
+    return drivers.filter(drivers=>{
+        return drivers.name===about;
+    })
+}
